48cdc15edf1827fbe4c367fcb238b254cd4b64cc
yangwm <jxfzywm@163.com>
2015-06-06 17:41:25 +0800

    mate-ios: add MateViewController.xib page

---
+++ mate-ios/mate-ios/MateViewController.m
@@ -0,0 +1,38 @@
+//
+//  MateViewController.m
+//  mate-ios
+//
+//  Created by yangwm on 6/6/15.
+//  Copyright (c) 2015 xcodecraft. All rights reserved.
+//
+
+#import "MateViewController.h"
+
+@interface MateViewController ()
+
+@end
+
+@implementation MateViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
