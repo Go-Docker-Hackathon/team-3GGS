35e3a2fc466b174b506ba29da3ea390fae383490
neoduan <76372893@qq.com>
2015-06-07 01:50:51 +0800

    fix sh

--- mate-go/build.sh
+++ mate-go/build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 ROOT_DIR=`pwd`
-=======
-ROOT_DIR="/gopath/app/team-3GGS/mate-go"
->>>>>>> ea134be5271c1f4f3e93a0a9c13f91ee2f4c6ccb
 cd $ROOT_DIR
 export GOPATH=$ROOT_DIR
 export GOBIN=$GOPATH/bin
